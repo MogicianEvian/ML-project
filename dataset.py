9c32f86599fc2f14f1ee64a3d1a046f79ed61a69
MogicianEvian <378442635@qq.com>
2021-06-28 01:07:43 +0800

    ddpm

--- dataset.py
+++ dataset.py
@@ -76,7 +76,7 @@ def get_dataset(dataset, datadir, augmentation=True, classes=None, ddpm=False):
         import numpy
         ddpm_dataset = numpy.load('./data/cifar10_ddpm.npz')
         a = ddpm_dataset['image']
-        c = numpy.zeros(a.shape[0],3,32,32)
+        c = numpy.zeros([a.shape[0],3,32,32], dtype = a.dtype)
         for i in range(a.shape[0]):
             for j in range(32):
                 for k in range(32):
