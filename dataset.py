4963bbddddff91acd978790a847a304671d85f5c
MogicianEvian <378442635@qq.com>
2021-06-28 00:17:22 +0800

    ddpm

--- dataset.py
+++ dataset.py
@@ -60,8 +60,8 @@ def get_dataset(dataset, datadir, augmentation=True, classes=None, ddpm=False):
     test_dataset = Dataset(root=datadir, train=False, download=True, transform=test_transform)
     if ddpm is True:
         import numpy
-        ddpm_data = numpy.load('./data/cifar10_ddpm.npz')
-        ddpm_dataset = TensorDataset(ddpm_data['image'],ddpm_data['label'])
+        ddpm_dataset = numpy.load('./data/cifar10_ddpm.npz')
+        ddpm_dataset = TensorDataset(torch.from_numpy(ddpm_dataset['image']),torch.from_numpy(ddpm_dataset['label']))
         train_dataset = ConcatDataset(train_dataset, ddpm_dataset)
         print('ddpm_load')
     if classes is not None:
